4d54e22b5deb1dee28201cad9370912f4e94f578
Tejesh Anand <tjanand@usc.edu>
2023-11-14 11:44:57 -0800

    debounce buttons

--- bomberman_top.v
+++ bomberman_top.v
@@ -17,7 +17,27 @@ output 	MemOE, MemWR, RamCS, QuadSpiFlashCS;
 
 
 /* Local Signals */ 
+reg [26:0]	DIV_CLK;
+wire        Reset, ClkPort;
+wire		board_clk, sys_clk;
+
+//Debouncer wires
+wire  		Left_SCEN;
+wire  		Up_SCEN;
+wire  		Right_SCEN;
+wire  		Down_SCEN;
+wire        Middle_SCEN;
+
 //TODO - clock divider here? 
+always @(posedge board_clk, posedge Reset) 	
+    begin							
+        if (Reset)
+		DIV_CLK <= 0;
+        else
+		DIV_CLK <= DIV_CLK + 1'b1;
+    end
+
+assign	sys_clk = board_clk; //Running at the full 100Mhz speed
 
 // Disable the two memories so that they do not interfere with the rest of the design.
 assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
@@ -25,5 +45,20 @@ assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
 assign board_clk = ClkPort;	 	
 
 //TODO instantiate modules
+
+ee201_debouncer #(.N_dc(25)) ee201_debouncer_left
+    (.CLK(sys_clk), .RESET(Reset), .PB(BtnL), .DPB( ), .SCEN(Left_SCEN), .MCEN( ), .CCEN( ));
+
+ee201_debouncer #(.N_dc(25)) ee201_debouncer_right
+    (.CLK(sys_clk), .RESET(Reset), .PB(BtnR), .DPB( ), .SCEN(Right_SCEN), .MCEN( ), .CCEN( ));
+
+ee201_debouncer #(.N_dc(25)) ee201_debouncer_up
+    (.CLK(sys_clk), .RESET(Reset), .PB(BtnU), .DPB( ), .SCEN(Up_SCEN), .MCEN( ), .CCEN( ));
+
+ee201_debouncer #(.N_dc(25)) ee201_debouncer_down
+    (.CLK(sys_clk), .RESET(Reset), .PB(BtnD), .DPB( ), .SCEN(Down_SCEN), .MCEN( ), .CCEN( ));
+
+ee201_debouncer #(.N_dc(25)) ee201_debouncer_middle
+    (.CLK(sys_clk), .RESET(Reset), .PB(BtnC), .DPB( ), .SCEN(Middle_SCEN), .MCEN( ), .CCEN( ));
 
 endmodule
