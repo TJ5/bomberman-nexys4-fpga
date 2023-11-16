dd7d36381770d39ae64d00387da65a320b34c210
Tejesh Anand <tjanand@usc.edu>
2023-11-15 21:01:30 -0800

    add vga signal outputs

--- bomberman_top.v
+++ bomberman_top.v
@@ -1,18 +1,14 @@
 module divider_top (   
-	MemOE, MemWR, RamCS, QuadSpiFlashCS,
-    ClkPort,                           // the 100 MHz incoming clock signal
-	BtnL, BtnU, BtnD, BtnR,            // the Left, Up, Down, and the Right buttons 		BtnL, BtnR,
-	BtnC                      // the center button (this is our reset in most of our designs)
-    //TODO add whatever needed for the VGA
+	output MemOE, MemWR, RamCS, QuadSpiFlashCS,
+    input ClkPort,                           // the 100 MHz incoming clock signal
+	input BtnL, BtnU, BtnD, BtnR,            // the Left, Up, Down, and the Right buttons 		BtnL, BtnR,
+	input BtnC,                      // the center button (this is our reset in most of our designs)
+    //VGA signals
+    output hSync, vSync, 
+    output [3:0] vgaR, vgaG, vgaB
+    
     );
 
-/*  INPUTS */
-input	ClkPort;	
-input   BtnL, BtnU, BtnD, BtnR, BtnC;	
-
-/*  OUTPUTS */
-// Control signals on Memory chips 	(to disable them)
-output 	MemOE, MemWR, RamCS, QuadSpiFlashCS;
 // TODO VGA signals
 
 
