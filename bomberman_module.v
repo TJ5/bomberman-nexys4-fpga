6d95e402c11676ee22692d765bac7342af6ef875
Franco <zarzosac@usc.edu>
2023-11-19 21:53:12 -0800

    Updated Bomberman motion and walls restriction

--- bomberman_module.v
+++ bomberman_module.v
@@ -8,7 +8,7 @@ module bomberman
     output bomberman_on,                                 //current pixel location is inside bomberman sprite? -> Goes to Top_module 
     output [11:0] rgb_out                                       //color of current pixel within the bomberman sprite -> Goes to Top_module
     );
-
+    
     /* INPUTS */
     wire clk, reset;
     wire [9:0] v_x, v_y;
@@ -22,13 +22,26 @@ module bomberman
 
 
     //pixel coordinate boundaries for VGA display area
-    localparam MAX_X = 640;
-    localparam MAX_Y = 480;
-    localparam MIN_Y =  16;
+    localparam MAX_X = 740;
+    localparam MIN_X = 144;
+    localparam MAX_Y = 500;
+    localparam MIN_Y =  144;
 
     //Bomberman tile width height
     localparam B_W = 16;
     localparam B_H = 16;
+    
+    //Wall locations relative to space taken up by Bomberman
+    localparam LEFT_WALL = MIN_X;
+    localparam RIGHT_WALL = MAX_X - B_W;
+    localparam TOP_WALL = MIN_Y;
+    localparam BOTTOM_WALL = MAX_Y - B_H;
+    
+    //Time that needs to be reached before Bomberman starts moving
+    localparam TIME_LIMIT = 100000;
+    
+ 
+ 
 
   
 // * FSM THAT UPDATES BOMBERMAN'S SPRITE LOCATION *//
@@ -46,18 +59,25 @@ localparam Idle = 4'b0000;
    assign {q_Left, q_Right, q_Up, q_Down} = movement_state;
 
     wire blocked_left, blocked_right, blocked_up, blocked_down;
-    assign {blocked_left, blocked_right, blocked_up, blocked_down} = bomberman_blocked;
+   // Assign individual bits
+    assign blocked_left  = bomberman_blocked[0];
+    assign blocked_right = bomberman_blocked[1];
+    assign blocked_up    = bomberman_blocked[2];
+    assign blocked_down  = bomberman_blocked[3];
    
+   //20 bit counter
+   reg[20:0] counter;
  
-    always @(posedge clk, posedge reset)
+    always @(posedge clk, posedge C)
         begin
-            if (reset)
+            if (C)
                 begin
                 //Initialize bomberman to corner of the map
-                b_x     <= 10'd144;                 
-                b_y     <= 10'd400;
+                b_x     <= MIN_X;                 
+                b_y     <= 400;
                 //Initialize movement state to idle
                 movement_state <= Idle;
+                
                 end
             
             else
@@ -65,17 +85,18 @@ localparam Idle = 4'b0000;
                     Idle:
                         begin
                         //NSL
-                        if (L)
+                        if (L&&(b_x >= LEFT_WALL))
                             movement_state <= Left;
-                        else if (R)
+                        else if (R && (b_x <= RIGHT_WALL))
                             movement_state <= Right;
-                        else if (U)
+                        else if (U && (b_y >= TOP_WALL))
                             movement_state <= Up;
-                        else if (D)
+                        else if (D && (b_y <= BOTTOM_WALL))
                             movement_state <= Down;
                         //else we stay idle
 
                         //RTL - b_x and b_y don't need to be updated
+                            counter <=0;
                         end
                     Left:
                         begin
@@ -85,10 +106,15 @@ localparam Idle = 4'b0000;
                         //else we stay left
 
                         //RTL
-                        if(!blocked_left && !game_over)
+                        if(!blocked_left && !game_over && (b_x >= LEFT_WALL) && (counter ==100000))
                             b_x <= b_x - 1;
                         else
                             b_x <= b_x;
+                            
+                        if (counter == 100000)
+                            counter <= 0;
+                        else
+                            counter <= counter +1;
                         end
     
                     Right:
@@ -99,11 +125,16 @@ localparam Idle = 4'b0000;
                         //else we stay right
 
                         //RTL
-                        if(!blocked_right && !game_over)
+                        if(!blocked_right && !game_over && (b_x <= RIGHT_WALL) && (counter==100000))
                             b_x <= b_x + 1;
                         else
                             b_x <= b_x;
-                        end
+                        if (counter == 100000)
+                            counter <= 0;
+                        else
+                            counter <= counter +1;
+                        
+                        end                                       
                     Down:
                         begin
                         //NSL
@@ -112,10 +143,16 @@ localparam Idle = 4'b0000;
                         //else we stay down
 
                         //RTL
-                        if(!blocked_down && !game_over)
-                            b_y <= b_y - 1;
+                        if(!blocked_down && !game_over && (b_y <= BOTTOM_WALL) && (counter==100000))
+                            b_y <= b_y + 1;
                         else
                             b_y <= b_y;
+                            
+                        if (counter == 100000)
+                            counter <= 0;
+                        else
+                            counter <= counter +1;
+                                                
                         end
                     Up:
                         begin
@@ -125,10 +162,15 @@ localparam Idle = 4'b0000;
                         //else we stay up
 
                         //RTL
-                        if(!blocked_up && !game_over)
-                            b_y <= b_y + 1;
+                        if(!blocked_up && !game_over && (b_y >= TOP_WALL) && (counter==100000))
+                            b_y <= b_y - 1;
                         else
                             b_y <= b_y;
+                        if (counter == 100000)
+                            counter <= 0;
+                        else
+                            counter <= counter +1;                        
+                                    
                         end
                         
                     default:		
@@ -145,6 +187,6 @@ assign row = v_y - b_y; // row of current pixel within bomberman sprite
 bomberman_rom bm_rom_unit(.clk(clk), .row(row), .col(col), .color_data(rgb_out));
            
 // Notify top_module that current pixel is inside of bomberman's sprite, so it should display the rgb_out from the bombermman module 
-assign bomberman_on = (v_x >= b_x) & (v_x <= b_x + B_W - 1) & (v_y >= b_y) & (v_y <= b_y + B_H - 1);
+assign bomberman_on = (v_x >= b_x) && (v_x <= b_x + B_W - 1) && (v_y >= b_y) && (v_y <= b_y + B_H - 1);
 
 endmodule
